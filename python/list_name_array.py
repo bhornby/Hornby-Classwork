8a729db152827bc6a14c0dc71695df8f19704059
dc-carrolls <32237935+dc-carrolls@users.noreply.github.com>
2020-09-23 11:38:12 +0100

    Feedback: NameList
    There are a few issues here. The first is that user_choice is a string and you are comparing with an integer... The second is that you shouldn't use insert(), look in the code comments. Lastly, your function should be returning the value for user_choice.

--- python/list_name_array.py
+++ python/list_name_array.py
@@ -7,18 +7,21 @@ def displayMenu():
 #end function
 choice = displayMenu()    
 
-user_choice =(input("Enter your choice: "))
+
+user_choice =(input("Enter your choice: ")) 
 
 name_list = [1,2,3,4,5,6,7,8,9,10]
-
+### SRC - When you are doing validation, you need to have a
+### loop. Something like while choice not valid:
 if int(user_choice)>3 and int(user_choice)<1:
     user_choice =(input("Invalid choice - please re-enter: "))
     
-elif user_choice == 1:
+elif user_choice == 1: ### SRC - user_choice is still a string and you are comparing to an interger
     print("Enter the name to be added to the list")
     name = input()
     print("Enter the name to be added to the list to insert the name (1 - 10):")
     posititon = input()
+    ### SRC - Don't use insert. Use name_list[position] = name
     name_list.insert(position, name)
     
 elif user_choice == 2:
