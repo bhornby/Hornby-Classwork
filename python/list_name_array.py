00f96fa009f79a77f4263244e38cc1016f36d052
bhornby <70747776+bhornby@users.noreply.github.com>
2020-09-23 19:48:12 +0100

    changes folowing feed back

--- python/list_name_array.py
+++ python/list_name_array.py
@@ -8,21 +8,33 @@ def displayMenu():
 choice = displayMenu()    
 
 
-user_choice =(input("Enter your choice: ")) 
+user_choice =(input("Enter your choice: "))
+user_choice = int(user_choice)
+v_user_choice = False
 
 name_list = [1,2,3,4,5,6,7,8,9,10]
 ### SRC - When you are doing validation, you need to have a
 ### loop. Something like while choice not valid:
-if int(user_choice)>3 and int(user_choice)<1:
-    user_choice =(input("Invalid choice - please re-enter: "))
+
+while not v_user_choice:
+    print("Invalid choice - please re-enter: ")
+    user_choice =int(input())
+    if user_choice <= 3 and user_choice >0:
+        v_user_choice = True
+    #end if
+#end while
     
-elif user_choice == 1: ### SRC - user_choice is still a string and you are comparing to an interger
+if user_choice == 1: ### SRC - user_choice is still a string and you are comparing to an interger
     print("Enter the name to be added to the list")
     name = input()
-    print("Enter the name to be added to the list to insert the name (1 - 10):")
-    posititon = input()
+    print("Enter the desired posistion in the list (1 - 10):")
+    position = int(input())
+    position = (position - 1)
     ### SRC - Don't use insert. Use name_list[position] = name
-    name_list.insert(position, name)
+    name_list[position] = name
+    for nam in name_list:
+        print(nam)
+    #end for
     
 elif user_choice == 2:
     for nam in name_list:
@@ -32,7 +44,7 @@ elif user_choice == 2:
 elif user_choice == 3:
     print("program terminating")
 
-
+#end if
 
     
     
