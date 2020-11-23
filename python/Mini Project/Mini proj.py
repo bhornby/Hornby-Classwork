c4d30600ccfafae2562b7ebdabe8830b718da944
bhornby <70747776+bhornby@users.noreply.github.com>
2020-11-23 08:51:52 +0000

    movement in y not working very confused
    went back a version to add a proper update function but now im getting an error that my player class has no attribute speed.

---
+++ python/Mini Project/Mini proj.py
@@ -0,0 +1,146 @@
+import pygame
+import random
+
+#defining colours
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED = (255,0,0,)
+BLUE = (0,0,139)
+YELLOW = (210,255,0)
+
+#black screen
+size = (1000,720)
+
+screen = pygame.display.set_mode(size)
+
+#title of the window
+pygame.display.set_caption("MINI Project")
+
+#exit game falg set to false
+done = False
+
+#screen refresh rate
+clock = pygame.time.Clock()
+
+class Player(pygame.sprite.Sprite):  #blueprint for an object with methods and attribute #and object is an instance of a class
+    def __init__(self,colour,width,height,x,y):
+        super().__init__()
+        
+        #set player dimentions
+        self.speed.x = x
+        self.speed.y = y
+        self.image = pygame.Surface([width,height])
+        self.image.fill(colour)
+        
+        #set position of player
+        self.rect = self.image.get_rect()
+        self.rect.x = 360
+        self.rect.y = 500
+        
+    def update(self):
+        self.rect.x = self.rect.x + self.speed.x
+        self.rect.y = self.rect.y + self.speed.y
+            
+    def player_set_speed(self,x,y):
+        self.speed.x = x
+        self.speed.y = y
+        
+class Wall(pygame.sprite.Sprite):
+    def __init__(self,colour,width,height,x,y):
+        super().__init__()
+        self.image = pygame.Surface([width,height])
+        self.image.fill(colour)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+    #end procedure
+#end class
+
+map = [
+       [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+       [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+       [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+       [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+       [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+       [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+       [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+       [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+       [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+       [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+       [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+       [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+       [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+       [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+       [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+       [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+       [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+       [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    ]
+
+all_sprite_group = pygame.sprite.Group()
+
+my_player = Player(YELLOW,40,40,0,0)
+all_sprite_group.add(my_player)
+            
+wall_group = pygame.sprite.Group()
+
+x = 0
+y = 0
+
+for row in map:
+    for column in row:
+        if column == 1:
+            my_wall = Wall(BLUE,40,40,x,y)
+            all_sprite_group.add(my_wall)
+            wall_group.add(my_wall)
+        x = x + 40
+        #end if
+    #next column
+    x = 0
+    y = y + 40
+#next row
+
+'''wall_hit_list = pygame.sprite.spritecollide(my_player, wall_group, True)
+    for x in wall_hit_list:
+        if x'''
+
+pygame.init()
+#game loop
+while not done:
+    #user input
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+            
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:#if left key is pressed
+                my_player.player_set_speed(-10,0)
+            elif event.key == pygame.K_RIGHT:
+                my_player.player_set_speed(10,0)
+            elif event.key == pygame.K_UP:
+                my_player.player_set_speed(0,-10)
+            elif event.key == pygame.K_DOWN:
+                my_player.player_set_speed(0,10)
+  
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
+                my_player.player_set_speed(0) 
+
+
+    
+    
+    #update all sprites    
+    all_sprite_group.update()
+    #screen background is black
+    screen.fill(BLACK)
+    #draw function
+    all_sprite_group.draw(screen)
+    #flip display to show new position of objects
+    
+    
+    
+    pygame.display.flip()
+    clock.tick(60)
+    
+#end game loop
+pygame.quit()
